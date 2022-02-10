f644ee669cb60dc359dab5eca82f6f2342a3ea9e
Joanna Gorska <gorska919@gmail.com>
2022-02-10 19:23:48 +0000

    Add wikipedia spider

--- article_scraper/article_scraper/items.py
+++ article_scraper/article_scraper/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class ArticleScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Article(scrapy.Item):
+    title = scraply.Field()
+    url = scrapy.Field()
+    lastUpdated = scrapy.Field()
