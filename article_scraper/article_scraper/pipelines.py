93e4ebe933e8dc08ce35388673cbe57b900e7887
Joanna Gorska <gorska919@gmail.com>
2022-02-10 20:12:23 +0000

    Add comments in pipeline and run spider again

--- article_scraper/article_scraper/pipelines.py
+++ article_scraper/article_scraper/pipelines.py
@@ -5,20 +5,32 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-from scrapy.exceptions import DropItem
 from datetime import datetime
+from scrapy.exceptions import DropItem
 
 
 class CheckItemPipeline:
+    '''
+    checks for missing items in received data
+    '''
     def process_item(self, article, spider):
+        '''
+        checkes for missing items and removes the whole object if any of the
+        items are missing
+        '''
         if not article['lastUpdated'] or not article['url'] or not article['title']:
             raise DropItem('Missing something!')
         return article
 
 
 class CleanDatePipeline:
-    def proces_item(self, article,spider):
+    '''
+    Changes the date for last updated to a python datetime object
+    '''
+    def proces_item(self, article, spider):
+        '''
+        removes the text from the date and changes it into python date time object
+        '''
         article['lastUpdated'].replace('This page was last edited on', '').strip()
         article['lastUpdated'] = datetime.strptime(article['lastUpdated'], '%d %B %Y, at %H:%M')
         return article
