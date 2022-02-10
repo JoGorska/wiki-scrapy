89065d2166bc9cf56ca486c3c0c03f1d66338241
Joanna Gorska <gorska919@gmail.com>
2022-02-10 19:18:58 +0000

    Inital commit

---
+++ article_scraper/article_scraper/spiders/wikipedia.py
@@ -0,0 +1,19 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+
+class WikipediaSpider(CrawlSpider):
+    name = 'wikipedia'
+    allowed_domains = ['en.wikipedia.org']
+    start_urls = ['https://en.wikipedia.org/wiki/Kevin_Bacon']
+
+    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)]
+
+    def parse_info(self, response):
+        article = Article()
+        article['title'] = response.xpath('//h1/text()').get() or response.xpath('//h1/i/text()').get(),
+        article['url'] = response.url
+        article['lastUpdated'] = response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
+        
+        return article
